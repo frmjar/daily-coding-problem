daecf88a89366ba7f3e0d8c978589256f888989f
Felix Redondo <frmanzanares@hotmail.com>
2021-11-07 20:40:33 +0100

    Solution Problem 12

---
+++ solutions/JavaScript/problem_012.js
@@ -0,0 +1,16 @@
+function ways (n) {
+  if (n === 0) { return 1 }
+  if (n < 0) { return 0 }
+  return ways(n - 1) + ways(n - 2)
+}
+function waysVariableSteps (n, steps) {
+  if (n === 0) { return 1 }
+  if (n < 0) { return 0 }
+  let ways = 0
+  for (let i = 0; i < steps.length; i++) {
+    ways += waysVariableSteps(n - steps[i], steps)
+  }
+  return ways
+}
+console.log(ways(4))
+console.log(waysVariableSteps(4, [1, 3, 5]))
